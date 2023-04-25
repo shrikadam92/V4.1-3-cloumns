3a1d0e9a6661a4806ca60597211a9b21f400d841
shrikadam92 <129550251+shrikadam92@users.noreply.github.com>
2023-04-25 21:42:52 +0530

    remove

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
 // This script runs in the background of your extension and can handle long-running or asynchronous tasks.
 
 // This example listens for the onInstalled event and logs a message to the console.
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({ color: '#3aa757' }, function() {
-    console.log('The color is green.');
-  });
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {action: "paste"});
 });
